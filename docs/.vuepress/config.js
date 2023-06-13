d00ed836a7e757079827b4cf44891ee1d8fed59b
zhang <31178293+helloonetwo@users.noreply.github.com>
2023-06-13 17:21:31 +0800

    100

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 module.exports = {
   title: '前端知识体系',
-  base: '/interview/',
+  base: '/',
   description: '前端知识体系',
   // markdown: {
   //   // markdown-it-anchor 的选项
